fd996d9cc5fc5e3ffec0da080bb989ae8654a3f5
raizen4 <boldurbogdan@yahoo.com>
2016-03-19 08:04:07 +0000

    patch 0.2

--- Main.py
+++ Main.py
@@ -2,6 +2,7 @@ import pygame
 from pygame.locals import  *
 import sys
 import numpy
+import random
 class Player(pygame.sprite.Sprite):
     def __init__(self,name,image):
         pygame.sprite.Sprite.__init__(self)
@@ -9,6 +10,7 @@ class Player(pygame.sprite.Sprite):
         self.image_name=image
         self.image=pygame.image.load(self.image_name).convert()
         self.rect=self.image.get_rect()
+
     def set_position_on_grid(self,x_pos,y_pos):
         self.rect.x=x_pos
         self.rect.y=y_pos
@@ -38,14 +40,17 @@ class Player(pygame.sprite.Sprite):
             surface_for_UI.blit(main_screen,(0,0))
             pygame.display.flip()
 
-
-
-class Walls(Player):
-    def __int__(self,name,image,effect):
-        Player.__init__(self,name,image)
+class Traps(pygame.sprite.Sprite):
+    def __init__(self,name,image,effect=None):
+        pygame.sprite.Sprite.__init__(self)
         self.name=name
         self.image_name=image
         self.image=pygame.image.load(self.image_name).convert_alpha()
+        self.rect=self.image.get_rect()
+    def set_pos(self,x_pos,y_pos):
+        self.rect.x=x_pos
+        self.rect.y=y_pos
+
 
 "--------------------------global variables-------------------"
 size_of_main_screen=(1280,720) #latime,inaltime
@@ -54,6 +59,8 @@ surface_for_UI=pygame.Surface((240,720))
 surface_for_UI.fill((20,20,20))
 background=pygame.image.load("Background1.0g.png").convert()
 player_group=pygame.sprite.Group()
+traps_group=pygame.sprite.Group()
+power_ups_group=pygame.sprite.Group()
 
 "------displaying general things on the screen---------"
 player=Player("player1","images/tile-80px.png")
@@ -66,10 +73,13 @@ player_group.draw(main_screen)
 main_screen.blit(surface_for_UI,(0,0))
 pygame.display.flip()
 
-
+trap=Traps("trap","images/bear-trap1.bmp")
+trap.set_pos(0,0)
+traps_group.add(trap)
 
 while True:
-
+    traps_group.draw(main_screen)
+    pygame.display.flip()
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             sys.exit()
